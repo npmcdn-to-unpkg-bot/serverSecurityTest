31a4ad6da70bebe073db461cb146a0311d6552a8
sebhero <trevligt.att.ses@gmail.com>
2016-04-18 12:07:53 +0200

    addd jwt example

---
+++ demoJwt/src/main/java/com/example/ApiController.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by seb on 2016-04-18.
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "role/{role}", method = RequestMethod.GET)
+	public Boolean login(@PathVariable final String role,
+	                     final HttpServletRequest request) throws ServletException {
+		final Claims claims = (Claims) request.getAttribute("claims");
+
+		return ((List<String>) claims.get("roles")).contains(role);
+	}
+
+
+}
---
+++ demoJwt/src/main/java/com/example/DemoJwtApplication.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
+public class DemoJwtApplication {
+
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/api/*");
+
+		return registrationBean;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJwtApplication.class, args);
+	}
+}
