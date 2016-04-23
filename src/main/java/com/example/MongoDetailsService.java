128eb1b68eb51a26570c417c1075ab03ee3e0814
seb <trevligt.att.ses@gmail.com>
2016-04-23 13:26:29 +0200

    clean up code

--- src/main/java/com/example/MongoDetailsService.java
+++ src/main/java/com/example/MongoDetailsService.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
  * Created by Sebastian Börebäck on 2016-04-19.
  */
 @Service
-public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+public class MongoDetailsService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository repository;
--- src/test/java/com/example/ServerSecuritytestApplicationTests.java
+++ src/test/java/com/example/ServerSecuritytestApplicationTests.java
@@ -1,18 +1,73 @@
 package com.example;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ServerSecuritytestApplication.class)
 @WebAppConfiguration
 public class ServerSecuritytestApplicationTests {
 
-	@Test
-	public void contextLoads() {
+
+
+	private MockMvc mockMvc;
+
+
+	@Autowired
+	private ApplicationContext context;
+
+	private Authentication authentication;
+	private AuthenticationManager authenticationManager;
+
+	@Before
+	public void setUp() throws Exception {
+
+//		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//				.build();
+
+		authenticationManager = this.context
+				.getBean(AuthenticationManager.class);
 	}
 
+	@After
+	public void close() {
+		SecurityContextHolder.clearContext();
+	}
+
+	@Test
+	public void authenticated() throws Exception {
+		this.authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken("user", "pass"));
+		SecurityContextHolder.getContext().setAuthentication(this.authentication);
+	}
+
+	@Test(expected=BadCredentialsException.class)
+	public void failAuthenticated() throws Exception {
+		this.authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken("user", "wrongPassword"));
+
+		SecurityContextHolder.getContext().setAuthentication(this.authentication);
+
+
+
+		// TODO: 2016-04-22 :18:49 to understand assertThat and is var's
+		//they are static imports
+//		import static org.hamcrest.Matchers.is;
+//		import static org.junit.Assert.assertThat;
+//		assertThat(123, is("Greetings from Spring Boot!"));
+
+	}
 }
